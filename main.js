f8123e79b8f59fbb43e2d30441eaacc9293d2d81
bello ibraheem adewale <belloibraheem2003@gmail.com>
2023-12-23 05:43:42 +0100

    quiz application first commit

---
+++ main.js
@@ -0,0 +1,76 @@
+const quizData = [
+    {
+        question: 'what is the capital of Nigeria ?',
+        Option: ['Abuja','Paris','London','Tokyo'],
+        answer: 'paris'
+    },
+
+
+    {
+        question: 'what is the largest planet in our solar system ?',
+        Option: ['mars','saturn','jupiter','neptune'],
+        answer: 'jupiter'
+
+    },
+
+    {
+        question: 'who is the first women to drive a car in Nigeria',
+        Option: ['dev Nike','Mrs olufunmilayo Rasome Kuti', 'Dr Bee the developer','Mrs awujoola para'],
+        answer: 'Mrs olufunmilayo Rasome Kuti',
+    },
+
+    {
+
+    
+
+    question: 'which won the FIFA World cup in 2018',
+    Option: ['Nigeria','France','Argentina','Germany'],
+    answer: 'France'
+
+    },
+
+    {
+        question: 'what is the unit of Power',
+        Option: ['m/s','joules','area','metre'],
+        answer: 'joules',
+
+    },
+
+    {
+        question: 'In Javascript a call-back is also known as ......',
+        Option: ['paramiterise function','non-parmiterise function','Anonymous function'],
+        answer: 'Anonymous function'
+
+    },
+
+
+    {
+        question: 'The process of maintaining fairly constant in internal enivironment of an organism is caalled',
+        Option: ['Homestasis','Adaptation','Evolution','Predator'],
+        answer: 'Homestasis'
+
+
+    },
+
+    {
+        question: 'The first federal university in Nigeria is ',
+        Option: ['univrsity of Ilorin','university of lagos','university of ibadan','oxford university'],
+        answer: 'university of ibadan'
+    },
+
+
+    {
+        question: 'what is the chemical symbol of gold?',
+        Option: ['Ag','cu','Fe','Au'],
+        answer: 'Au'
+
+    },
+
+    {
+        question: 'how many local governmennt do we have in Oyo state',
+        Option: ["32","35","33","23"],
+        answer: "33"
+    }
+    
+] ;
+
